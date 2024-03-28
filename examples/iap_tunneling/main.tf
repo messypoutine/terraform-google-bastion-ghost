41561fb67cd291b5e01cd78e30e67f0d0e57a9f0
François Proulx <francois@boostsecurity.io>
2024-03-27 21:18:22 -0400

    First commit

---
+++ examples/iap_tunneling/main.tf
@@ -0,0 +1,93 @@
+/**
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+resource "google_compute_network" "network" {
+  project                 = var.project_id
+  name                    = "test-network-iap"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "subnet" {
+  project                  = var.project_id
+  name                     = "test-subnet-iap"
+  region                   = "us-west1"
+  ip_cidr_range            = "10.127.0.0/20"
+  network                  = google_compute_network.network.self_link
+  private_ip_google_access = true
+}
+
+resource "google_service_account" "vm_sa" {
+  project      = var.project_id
+  account_id   = "iap-test-instance"
+  display_name = "Service Account for VM"
+}
+
+# A testing VM to allow OS Login + IAP tunneling.
+module "instance_template" {
+  source  = "messypoutine/some-vm/google//modules/instance_template"
+  version = "~> 1.0"
+
+  project_id   = var.project_id
+  machine_type = "n1-standard-1"
+  subnetwork   = google_compute_subnetwork.subnet.self_link
+  service_account = {
+    email  = google_service_account.vm_sa.email
+    scopes = ["cloud-platform"]
+  }
+  metadata = {
+    enable-oslogin = "TRUE"
+  }
+}
+
+resource "google_compute_instance_from_template" "vm" {
+  name    = "iap-test-instance"
+  project = var.project_id
+  zone    = "us-west1-a"
+  network_interface {
+    subnetwork = google_compute_subnetwork.subnet.self_link
+  }
+  source_instance_template = module.instance_template.self_link
+}
+
+# Additional OS login IAM bindings.
+# https://cloud.google.com/compute/docs/instances/managing-instance-access#granting_os_login_iam_roles
+resource "google_service_account_iam_binding" "sa_user" {
+  service_account_id = google_service_account.vm_sa.id
+  role               = "roles/iam.serviceAccountUser"
+  members            = var.members
+}
+
+resource "google_project_iam_member" "os_login_bindings" {
+  for_each = toset(var.members)
+  project  = var.project_id
+  role     = "roles/compute.osLogin"
+  member   = each.key
+}
+
+module "iap_tunneling" {
+  source  = "messypoutine/bastion-ghost/google//modules/iap-tunneling"
+  version = "~> 1.0"
+
+  fw_name_allow_ssh_from_iap = "test-allow-ssh-from-iap-to-tunnel"
+  project                    = var.project_id
+  network                    = google_compute_network.network.self_link
+  service_accounts           = [google_service_account.vm_sa.email]
+  instances = [{
+    name = google_compute_instance_from_template.vm.name
+    zone = "us-west1-a"
+  }]
+  members = var.members
+}
---
+++ examples/simple_example/main.tf
@@ -0,0 +1,58 @@
+/**
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+module "iap_bastion" {
+  source  = "messypoutine/bastion-ghost/google"
+  version = "~> 1.0"
+
+  project = var.project_id
+  zone    = "us-west1-a"
+  network = google_compute_network.network.self_link
+  subnet  = google_compute_subnetwork.subnet.self_link
+  members = var.members
+  disk_labels = {
+    "test-label" = "test-value"
+  }
+}
+
+resource "google_compute_network" "network" {
+  project                 = var.project_id
+  name                    = "test-network"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "subnet" {
+  project                  = var.project_id
+  name                     = "test-subnet"
+  region                   = "us-west1"
+  ip_cidr_range            = "10.127.0.0/20"
+  network                  = google_compute_network.network.self_link
+  private_ip_google_access = true
+}
+
+resource "google_compute_firewall" "allow_access_from_bastion" {
+  project = var.project_id
+  name    = "allow-bastion-ssh"
+  network = google_compute_network.network.self_link
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  # Allow SSH only from IAP Bastion
+  source_service_accounts = [module.iap_bastion.service_account]
+}
